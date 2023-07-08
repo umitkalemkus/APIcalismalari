faff7118bed4bd4d8f9ece8dd04df189ee345a43
umitkalemkus <umitkalemkus@yahoo.com>
2023-07-08 23:31:08 +0100

    done

--- src/test/java/test/Get01.java
+++ src/test/java/test/Get01.java
@@ -1,4 +1,55 @@
 package test;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+
 public class Get01 {
+
+    public static void main(String[] args) {
+
+
+        String URL = "https://restful-booker.herokuapp.com/booking/7";
+
+
+        Response response = given().when().get(URL);
+        System.out.println("Response");
+        response.prettyPrint();
+
+        response.then().assertThat().statusCode(200);
+        response.then().assertThat().contentType("application/json");
+        response.then().assertThat().statusLine("HTTP/1.1 200 OK");
+        response.then().assertThat().contentType(ContentType.JSON);
+
+        System.out.println("Status code:  "+ response.getStatusCode());
+        System.out.println("Status headers:  "+ response.getHeaders());
+        System.out.println("Status body:  "+ response.getBody());
+        System.out.println("Status body:  "+ response.contentType());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
 }
